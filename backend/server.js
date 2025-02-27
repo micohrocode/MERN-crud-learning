5edc25c9cacb26a892615e985560d459d6d16d64
micohrocode <michaelrocode@gmail.com>
2025-02-27 11:12:01 -0500

    created a post endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,30 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
 
 dotenv.config();
 
 const app = express();
 
-app.get("/products", (req,res)=> {
-    res.send("Server is ready")
+app.use(express.json()) // accept and parse json data in the body
+
+app.post("/api/products", async (req,res)=> {
+    const product = req.body;
+
+    if(!product.name || !product.price || !product.image){
+        return res.status(400).json({success:false,message:"Please provide all fields"})
+    }
+
+    const newProduct = await new Product(product)
+
+    try {
+        await newProduct.save();
+        res.status(201).json({success: true, data: newProduct})
+    } catch (error) {
+        console.error("Error in creating product: ",error.message)
+        res.status(500).json({success: false, message: "Server Error"})
+    }
 })
 
 app.listen(5000, ()=> {
