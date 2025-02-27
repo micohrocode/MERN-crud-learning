fa475a36365d07ce69cd1db2b7205688749d5101
micohrocode <michaelrocode@gmail.com>
2025-02-27 11:33:07 -0500

    created an update endpoint

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import Product from "./models/product.model.js";
+import mongoose from "mongoose";
 
 dotenv.config();
 
@@ -34,6 +35,24 @@ app.post("/api/products", async (req,res)=> {
     } catch (error) {
         console.error("Error in creating product: ",error.message)
         res.status(500).json({success: false, message: "Server Error"})
+    }
+})
+
+app.put("/api/products/:id", async (req,res) => {
+    const {id} =  req.params
+
+    const product = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({success: false, message: "Invalid product Id"})
+    }
+
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(id, product, {new: true})
+        res.status(200).json({ success: true, data: updatedProduct})
+    } catch (error) {
+        console.error("Error updating product: ",error.message)
+        res.status(500).json({success: true, message: "Server Error"})
     }
 })
 
