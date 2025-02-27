edfda66c2ba2f15e3342d23969710f2c6e54d607
micohrocode <michaelrocode@gmail.com>
2025-02-27 11:25:28 -0500

    created a read endpoint

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,16 @@ const app = express();
 
 app.use(express.json()) // accept and parse json data in the body
 
+app.get("/api/products", async (req,res) => {
+    try {
+        const products = await Product.find({})
+        res.status(200).json({ success: true, data: products})
+    } catch (error) {
+        console.error("error in fetching products: ",error.message)
+        res.status(500).json({success: false, message: "Server Error"})
+    }
+})
+
 app.post("/api/products", async (req,res)=> {
     const product = req.body;
 
@@ -34,6 +44,7 @@ app.delete("/api/products/:id", async (req,res) => {
         await Product.findByIdAndDelete(id);
         res.status(200).json({success:true, message: "Product deleted"})
     } catch (error) {
+        console.error("Error in deleting product: ", error.message)
         res.status(404).json({success: false, message: "Product not found"})
     }
 })
