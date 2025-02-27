eee3ce66ec418eba0998ea1c969745b6dd9ad274
micohrocode <michaelrocode@gmail.com>
2025-02-27 13:23:57 -0500

    react router dom setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { ChakraProvider } from "@chakra-ui/react"
 import { system } from "@chakra-ui/react/preset";
+import {BrowserRouter} from "react-router-dom"
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ChakraProvider value={system}>
-      <App />
-    </ChakraProvider>
+    <BrowserRouter>
+      <ChakraProvider value={system}>
+          <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
