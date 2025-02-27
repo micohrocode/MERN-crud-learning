5ad4fea195c478ae198de5400d4c94ff47347be7
micohrocode <michaelrocode@gmail.com>
2025-02-27 13:48:44 -0500

    added navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,33 @@
+import { Button, Container, Flex, HStack, Text  } from '@chakra-ui/react'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+
   return (
-    <div>Navbar</div>
+    <Container maxW="1140px" px={4}>
+        <Flex
+            h={16}
+            alignItems={"center"}
+            justifyContent={"space-between"}
+            flexDir={{
+                base:"column",
+                sm:"row"
+            }}
+        >
+
+            
+            <Link to={"/"}>Product Store</Link>
+
+            <HStack spacing={2} alignItems={"center"}>
+                <Link to="/create">
+                    <Button>
+                        +
+                    </Button>
+                </Link>
+            </HStack>
+        </Flex>
+    </Container>
   )
 }
 
